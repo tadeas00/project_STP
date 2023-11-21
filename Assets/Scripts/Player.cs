29c47cb4e1c32d6693a7b63f614c3d8eecf1709b
Tadeas120 <77239884+tadeas00@users.noreply.github.com>
2023-11-21 22:07:27 +0100

    Base of collisons and camera/movment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private BoxCollider2D boxCollider;
+   private Vector3 moveDelta;
+   private RaycastHit2D hit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+      boxCollider = GetComponent<BoxCollider2D>();
+   }
+
+   private void FixedUpdate()
+   {
+      float x = Input.GetAxisRaw("Horizontal");
+      float y = Input.GetAxisRaw("Vertical");
+      
+      moveDelta = new Vector3(x, y, 0);
+      
+      // Prohození směru postavy, dle toho kam jde
+      if (moveDelta.x > 0)
+         transform.localScale = Vector3.one;
+      else if (moveDelta.x < 0)
+         transform.localScale = new Vector3(-1, 1, 1);
+      
+      hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+      if(hit.collider == null)
+      {
+         transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+      }
+
+      hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+      if(hit.collider == null)
+      {
+         transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+      }
+
+   }
 }

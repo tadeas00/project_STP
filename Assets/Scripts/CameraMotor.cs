112e34293b3795475dc2f983e3c4c0d3f689e586
Tadeas120 <77239884+tadeas00@users.noreply.github.com>
2024-01-12 09:47:44 +0100

    Debugging and last edits before handin

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
 {
-    public Transform lookAt;
+    private Transform lookAt;
     public float boundX = 0.15f;
     public float boundY = 0.05f;
 

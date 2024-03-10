e24d2db87592ea2c8ef6653bad5039f9425c0f83
Tadeas120 <77239884+tadeas00@users.noreply.github.com>
2024-03-10 19:23:23 +0100

    Ready dungeons, next animations and NPC

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dialog : Collidable
@@ -20,7 +19,18 @@ public class Dialog : Collidable
         if (Time.time - lastTalk > cooldown)
         {
             lastTalk = Time.time;
-            GameManager.instance.ShowText(DialogText, 25, Color.yellow, transform.position + new Vector3(0,0.16f,0), Vector3.zero, cooldown); 
+            StartCoroutine(DisplayDialog());
+        }
+    }
+
+    IEnumerator DisplayDialog()
+    {
+        Vector3 startingPosition = transform.position + new Vector3(0, 0.16f, 0);
+        for (int i = 0; i < DialogText.Length; i++)
+        {
+            Vector3 newPosition = startingPosition + new Vector3(i * 0.08f, 0, 0); // Adjust the spacing between letters as needed
+            GameManager.instance.ShowText(DialogText[i].ToString(), 25, Color.yellow, newPosition, Vector3.zero, cooldown); 
+            yield return new WaitForSeconds(0.05f); // Adjust the speed of typing by changing the WaitForSeconds value
         }
     }
 }
